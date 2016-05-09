a7df0a26683912f9a954d7066cb8a5c5d15a3f3c
WEBXICO-MX <weiss.uttab@gmail.com>
2016-05-09 12:52:53 -0500

    Versión inicial.

---
+++ index.php
@@ -0,0 +1,94 @@
+<!DOCTYPE html>
+<html lang="es">
+    <head>
+        <title>Home &#124; Corporativo Integral para Soluciones en Tiempo (CIPSET)</title>
+        <meta charset="UTF-8">        
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link href="bower_components/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet"/>
+        <style>
+            p { text-align: justify;}
+        </style>
+    </head>
+    <body>
+        <nav class="navbar navbar-inverse">
+            <div class="container">
+                <div class="navbar-header">
+                    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+                        <span class="sr-only">Toggle navigation</span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </button>
+                    <a class="navbar-brand" href="#">Corporativo Integral para Soluciones en Tiempo (CIPSET)</a>
+                </div>
+                <div id="navbar" class="collapse navbar-collapse">
+                    <ul class="nav navbar-nav">
+                        <li class="active"><a href="#">Inicio</a></li>
+                        <li><a href="#about">Conócenos</a></li>
+                        <li class="dropdown">
+                            <a class="dropdown-toggle" data-toggle="dropdown" href="#">Servicios <span class="caret"></span></a>
+                            <ul class="dropdown-menu">
+                                <li><a href="#">Capacitación</a></li>
+                                <li><a href="#">Asesoría</a></li>
+                                <li><a href="#">Proyectos</a></li>
+                                <li><a href="#">Obras</a></li>
+                            </ul>
+                        </li>
+                        <li><a href="#about">Galería</a></li>
+                        <li><a href="#about">Otros</a></li>
+                    </ul>
+                </div><!-- /.nav-collapse -->
+            </div><!-- /.container -->
+        </nav>
+        <div class="container">
+            <div class="row row-offcanvas row-offcanvas-right">
+                <div class="col-xs-12 col-sm-12">
+                    <p class="pull-right visible-xs">
+                        <button type="button" class="btn btn-primary btn-xs" data-toggle="offcanvas">Toggle nav</button>
+                    </p>
+                    <div class="jumbotron">
+                        <h1>Sitio web en contrucción</h1>
+                    </div>
+                    <div class="row">
+                        <div class="col-xs-6 col-lg-4">
+                            <h2>Encabezado</h2>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                            <p><a class="btn btn-primary" href="#" role="button">Ver detalles &raquo;</a></p>
+                        </div><!--/.col-xs-6.col-lg-4-->
+                        <div class="col-xs-6 col-lg-4">
+                            <h2>Encabezado</h2>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                            <p><a class="btn btn-primary" href="#" role="button">Ver detalles &raquo;</a></p>
+                        </div><!--/.col-xs-6.col-lg-4-->
+                        <div class="col-xs-6 col-lg-4">
+                            <h2>Encabezado</h2>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                            <p><a class="btn btn-primary" href="#" role="button">Ver detalles &raquo;</a></p>
+                        </div><!--/.col-xs-6.col-lg-4-->
+                        <div class="col-xs-6 col-lg-4">
+                            <h2>Encabezado</h2>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                            <p><a class="btn btn-primary" href="#" role="button">Ver detalles &raquo;</a></p>
+                        </div><!--/.col-xs-6.col-lg-4-->
+                        <div class="col-xs-6 col-lg-4">
+                            <h2>Encabezado</h2>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                            <p><a class="btn btn-primary" href="#" role="button">Ver detalles &raquo;</a></p>
+                        </div><!--/.col-xs-6.col-lg-4-->
+                        <div class="col-xs-6 col-lg-4">
+                            <h2>Encabezado</h2>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                            <p><a class="btn btn-primary" href="#" role="button">Ver detalles &raquo;</a></p>
+                        </div><!--/.col-xs-6.col-lg-4-->
+                    </div><!--/row-->
+                </div>
+            </div>
+            <hr>
+            <footer>
+                <p class="text-center">&copy; Copyright <?php echo date("Y"); ?> | Corporativo Integral para Soluciones en Tiempo | Powered By <a href="http://webxico.blogspot.mx/" target="_blank">WEBXICO</a></p>
+            </footer>
+        </div>
+        <script src="bower_components/jquery/dist/jquery.min.js"></script>
+        <script src="bower_components/bootstrap/dist/js/bootstrap.min.js"></script>
+    </body>
+</html>
