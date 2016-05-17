6dc6efa28cefea74253578e1d66d30d28edd484c
Roberto Eder Weiss Juárez <weiss.uttab@gmail.com>
2016-05-17 01:38:29 -0500

    Se crea galeria de imagenes y se muestran las clasificaciones de las capacitaciones en el index

---
+++ galeria.php
@@ -0,0 +1,167 @@
+<!DOCTYPE html>
+<html lang="es">
+    <head>
+        <title>Galería &#124; Corporativo Integral para Soluciones en Tiempo (CIPSET)</title>
+        <meta charset="UTF-8">        
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link href="bower_components/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet"/>
+        <link href="css/cipset.css" rel="stylesheet"/>
+    </head>
+    <body>
+        <nav class="navbar navbar-inverse">
+            <div class="container">
+                <div class="navbar-header">
+                    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+                        <span class="sr-only">Toggle navigation</span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </button>
+                    <a class="navbar-brand" href="index.php">Corporativo Integral para Soluciones en Tiempo (CIPSET)</a>
+                </div>
+                <div id="navbar" class="collapse navbar-collapse">
+                    <ul class="nav navbar-nav">
+                        <li><a href="index.php">Inicio</a></li>
+                        <li><a href="#about">Conócenos</a></li>
+                        <li class="dropdown">
+                            <a class="dropdown-toggle" data-toggle="dropdown" href="#">Servicios <span class="caret"></span></a>
+                            <ul class="dropdown-menu">
+                                <li><a href="#">Capacitación</a></li>
+                                <li><a href="#">Asesoría</a></li>
+                                <li><a href="#">Proyectos</a></li>
+                                <li><a href="#">Obras</a></li>
+                            </ul>
+                        </li>
+                        <li class="active"><a href="galeria.php">Galería</a></li>
+                        <li><a href="#about">Otros</a></li>
+                    </ul>
+                </div><!-- /.nav-collapse -->
+            </div><!-- /.container -->
+        </nav>
+        <div class="container">            
+            <div class="row">
+                <div class="col-md-12">
+                    <div id="miGaleria" class="carousel slide" data-ride="carousel">
+                        <!-- Indicators -->
+                        <ol class="carousel-indicators">
+                            <li data-target="#miGaleria" data-slide-to="0" class="active"></li>
+                            <li data-target="#miGaleria" data-slide-to="1"></li>
+                            <li data-target="#miGaleria" data-slide-to="2"></li>
+                            <li data-target="#miGaleria" data-slide-to="3"></li>
+                            <li data-target="#miGaleria" data-slide-to="4"></li>
+                            <li data-target="#miGaleria" data-slide-to="5"></li>
+                            <li data-target="#miGaleria" data-slide-to="6"></li>
+                            <li data-target="#miGaleria" data-slide-to="7"></li>
+                            <li data-target="#miGaleria" data-slide-to="8"></li>
+                            <li data-target="#miGaleria" data-slide-to="9"></li>
+                            <li data-target="#miGaleria" data-slide-to="10"></li>
+                            <li data-target="#miGaleria" data-slide-to="11"></li>
+                            <li data-target="#miGaleria" data-slide-to="12"></li>
+                            <li data-target="#miGaleria" data-slide-to="13"></li>
+                            <li data-target="#miGaleria" data-slide-to="14"></li>
+                            <li data-target="#miGaleria" data-slide-to="15"></li>
+                            <!--<li data-target="#miGaleria" data-slide-to="16"></li>
+                            <li data-target="#miGaleria" data-slide-to="17"></li>
+                            <li data-target="#miGaleria" data-slide-to="18"></li>
+                            <li data-target="#miGaleria" data-slide-to="19"></li>
+                            <li data-target="#miGaleria" data-slide-to="20"></li>
+                            <li data-target="#miGaleria" data-slide-to="21"></li>
+                            <li data-target="#miGaleria" data-slide-to="22"></li>
+                            <li data-target="#miGaleria" data-slide-to="23"></li>
+                            <li data-target="#miGaleria" data-slide-to="24"></li>
+                            <li data-target="#miGaleria" data-slide-to="25"></li>-->
+                        </ol>
+
+                        <!-- Wrapper for slides -->
+                        <div class="carousel-inner" role="listbox">
+                            <!--<div class="item active">
+                                <img src="img/galeria/01/IMG-20160511-WA0000-min.jpg" alt="imagen 01" class="img-responsive"/>
+                            </div>-->
+
+                            <div class="item active">
+                                <img src="img/galeria/01/IMG-20160511-WA0001-min.jpg" alt="imagen 02" class="img-responsive"/>
+                            </div>
+
+                            <div class="item">
+                                <img src="img/galeria/01/IMG-20160511-WA0002-min.jpg" alt="imagen 03" class="img-responsive"/>
+                            </div>
+
+                            <div class="item">
+                                <img src="img/galeria/01/IMG-20160511-WA0003-min.jpg" alt="imagen 04" class="img-responsive"/>
+                            </div>
+                            <div class="item">
+                                <img src="img/galeria/01/IMG-20160511-WA0004-min.jpg" alt=""/>
+                            </div>
+                            <div class="item">
+                                <img src="img/galeria/01/IMG-20160511-WA0005-min.jpg" alt=""/>
+                            </div>
+                            <div class="item">
+                                <img src="img/galeria/01/IMG-20160511-WA0006-min.jpg" alt=""/>
+                            </div>
+                            <div class="item">
+                                <img src="img/galeria/01/IMG-20160511-WA0007-min.jpg" alt=""/>
+                            </div>
+                            <div class="item">
+                                <img src="img/galeria/01/IMG-20160511-WA0008-min.jpg" alt=""/>
+                            </div>
+                            <div class="item">
+                                <img src="img/galeria/01/IMG-20160511-WA0009-min.jpg" alt=""/>
+                            </div>
+                            <div class="item">
+                                <img src="img/galeria/01/IMG-20160511-WA0010-min.jpg" alt=""/>
+                            </div>
+                            <div class="item">
+                                <img src="img/galeria/01/IMG-20160511-WA0011-min.jpg" alt=""/>
+                            </div>
+                            <div class="item">
+                                <img src="img/galeria/01/IMG-20160511-WA0012-min.jpg" alt=""/>
+                            </div>
+                            <!--<div class="item">
+                                <img src="img/galeria/01/IMG-20160511-WA0013-min.jpg" alt=""/>
+                            </div>
+                            <div class="item">
+                                <img src="img/galeria/01/IMG-20160511-WA0014-min.jpg" alt=""/>
+                            </div>-->
+                            <div class="item">
+                                <img src="img/galeria/01/IMG-20160511-WA0015-min.jpg" alt=""/>
+                            </div>
+                            <div class="item">
+                                <img src="img/galeria/01/IMG-20160511-WA0016-min.jpg" alt=""/>
+                            </div>
+                            <div class="item">
+                                <img src="img/galeria/01/IMG-20160511-WA0017-min.jpg" alt=""/>
+                            </div>
+                            <div class="item">
+                                <img src="img/galeria/01/IMG-20160511-WA0018-min.jpg" alt=""/>
+                            </div>
+                            <!--<div class="item">
+                                   <img src="img/galeria/01/IMG-20160511-WA0025-min.jpg" alt=""/>
+                            </div>-->
+                            <div class="item">
+                                <img src="img/galeria/01/IMG-20160511-WA0026-min.jpg" alt=""/>
+                            </div>                         
+                        </div>
+
+                        <!-- Left and right controls -->
+                        <a class="left carousel-control" href="#miGaleria" role="button" data-slide="prev">
+                            <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+                            <span class="sr-only">Previous</span>
+                        </a>
+                        <a class="right carousel-control" href="#miGaleria" role="button" data-slide="next">
+                            <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+                            <span class="sr-only">Next</span>
+                        </a>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+        <hr>
+        <footer>
+            <p class="text-center">&copy; Copyright <?php echo date("Y"); ?> | Corporativo Integral para Soluciones en Tiempo | Powered By <a href="http://webxico.blogspot.mx/" target="_blank">WEBXICO</a></p>
+        </footer>
+    </div>
+    <script src="bower_components/jquery/dist/jquery.min.js"></script>
+    <script src="bower_components/bootstrap/dist/js/bootstrap.min.js"></script>
+</body>
+</html>
--- index.php
+++ index.php
@@ -5,9 +5,7 @@
         <meta charset="UTF-8">        
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link href="bower_components/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet"/>
-        <style>
-            p { text-align: justify;}
-        </style>
+        <link href="css/cipset.css" rel="stylesheet"/>
     </head>
     <body>
         <nav class="navbar navbar-inverse">
@@ -19,11 +17,11 @@
                         <span class="icon-bar"></span>
                         <span class="icon-bar"></span>
                     </button>
-                    <a class="navbar-brand" href="#">Corporativo Integral para Soluciones en Tiempo (CIPSET)</a>
+                    <a class="navbar-brand" href="index.php">Corporativo Integral para Soluciones en Tiempo (CIPSET)</a>
                 </div>
                 <div id="navbar" class="collapse navbar-collapse">
                     <ul class="nav navbar-nav">
-                        <li class="active"><a href="#">Inicio</a></li>
+                        <li class="active"><a href="index.php">Inicio</a></li>
                         <li><a href="#about">Conócenos</a></li>
                         <li class="dropdown">
                             <a class="dropdown-toggle" data-toggle="dropdown" href="#">Servicios <span class="caret"></span></a>
@@ -34,53 +32,72 @@
                                 <li><a href="#">Obras</a></li>
                             </ul>
                         </li>
-                        <li><a href="#about">Galería</a></li>
+                        <li><a href="galeria.php">Galería</a></li>
                         <li><a href="#about">Otros</a></li>
                     </ul>
                 </div><!-- /.nav-collapse -->
             </div><!-- /.container -->
         </nav>
-        <div class="container">
+        <div class="container">            
             <div class="row row-offcanvas row-offcanvas-right">
                 <div class="col-xs-12 col-sm-12">
                     <p class="pull-right visible-xs">
                         <button type="button" class="btn btn-primary btn-xs" data-toggle="offcanvas">Toggle nav</button>
                     </p>
                     <div class="jumbotron">
-                        <h1>Sitio web en contrucción</h1>
+                        <h1>Sitio web en construcción</h1>
                     </div>
+            <div class="row">
+                <div class="col-md-12">
                     <div class="row">
-                        <div class="col-xs-6 col-lg-4">
-                            <h2>Encabezado</h2>
+                        <div class="col-sm-6 col-md-4">
+                            <h2>DESARROLLO HUMANO</h2>
+                            <img src="img/desarrollo_humano.jpg" alt="Desarrollo humano" class="img-responsive"/><br/>
                             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-                            <p><a class="btn btn-primary" href="#" role="button">Ver detalles &raquo;</a></p>
-                        </div><!--/.col-xs-6.col-lg-4-->
-                        <div class="col-xs-6 col-lg-4">
-                            <h2>Encabezado</h2>
+                            <p><a class="btn btn-primary" href="desarrollo_humano.php" role="button"><span class="glyphicon glyphicon-eye-open"></span> Ver detalles &raquo;</a></p>
+                        </div>
+                        <div class="col-sm-6 col-md-4">
+                            <h2>HERRAMIENTAS DE CALIDAD</h2>
+                            <img src="img/herramientas_calidad.jpg" alt="Herramientas de calidad" class="img-responsive"/><br/>
                             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-                            <p><a class="btn btn-primary" href="#" role="button">Ver detalles &raquo;</a></p>
-                        </div><!--/.col-xs-6.col-lg-4-->
-                        <div class="col-xs-6 col-lg-4">
-                            <h2>Encabezado</h2>
+                            <p><a class="btn btn-primary" href="#" role="button"><span class="glyphicon glyphicon-eye-open"></span> Ver detalles &raquo;</a></p>
+                        </div>
+                        <div class="clearfix visible-sm-block"></div>
+                        <div class="col-sm-6 col-md-4">
+                            <h2>LEAN MANUFACTURING</h2>
+                            <img src="img/lean_manufacturing.jpg" alt="LEAN MANUFACTURING" class="img-responsive"/><br/>
                             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-                            <p><a class="btn btn-primary" href="#" role="button">Ver detalles &raquo;</a></p>
-                        </div><!--/.col-xs-6.col-lg-4-->
-                        <div class="col-xs-6 col-lg-4">
-                            <h2>Encabezado</h2>
+                            <p><a class="btn btn-primary" href="#" role="button"><span class="glyphicon glyphicon-eye-open"></span> Ver detalles &raquo;</a></p>
+                        </div>
+                        <div class="clearfix visible-md-block visible-lg-block"></div>
+                        <div class="col-sm-6 col-md-4">
+                            <h2>SEGURIDAD INDUSTRIAL</h2>
+                            <img src="img/seguridad_industrial.jpg" alt="SEGURIDAD INDUSTRIAL" class="img-responsive"/><br/>
                             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-                            <p><a class="btn btn-primary" href="#" role="button">Ver detalles &raquo;</a></p>
-                        </div><!--/.col-xs-6.col-lg-4-->
-                        <div class="col-xs-6 col-lg-4">
-                            <h2>Encabezado</h2>
+                            <p><a class="btn btn-primary" href="#" role="button"><span class="glyphicon glyphicon-eye-open"></span> Ver detalles &raquo;</a></p>
+                        </div>
+                        <div class="clearfix visible-sm-block"></div>
+                        <div class="col-sm-6 col-md-4">
+                            <h2>PERFORACION</h2>
+                            <img src="img/perforacion.jpg" alt="PERFORACION" class="img-responsive"/><br/>
                             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-                            <p><a class="btn btn-primary" href="#" role="button">Ver detalles &raquo;</a></p>
-                        </div><!--/.col-xs-6.col-lg-4-->
-                        <div class="col-xs-6 col-lg-4">
-                            <h2>Encabezado</h2>
+                            <p><a class="btn btn-primary" href="#" role="button"><span class="glyphicon glyphicon-eye-open"></span> Ver detalles &raquo;</a></p>
+                        </div>
+                        <div class="col-sm-6 col-md-4">
+                            <h2>MANTENIMIENTO</h2>
+                            <img src="img/mantenimiento.jpg" alt="MANTENIMIENTO" class="img-responsive"/><br/>
                             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-                            <p><a class="btn btn-primary" href="#" role="button">Ver detalles &raquo;</a></p>
-                        </div><!--/.col-xs-6.col-lg-4-->
-                    </div><!--/row-->
+                            <p><a class="btn btn-primary" href="#" role="button"><span class="glyphicon glyphicon-eye-open"></span> Ver detalles &raquo;</a></p>
+                        </div>
+                        <div class="clearfix visible-sm-block"></div>
+                        <div class="clearfix visible-md-block visible-lg-block"></div>
+                        <div class="col-sm-6 col-md-4">
+                            <h2>SERVICIO DE APOYO A POZOS</h2>
+                            <img src="img/servicios_apoyo_pozos.jpg" alt="SERVICIO DE APOYO A POZOS" class="img-responsive"/><br/>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                            <p><a class="btn btn-primary" href="#" role="button"><span class="glyphicon glyphicon-eye-open"></span> Ver detalles &raquo;</a></p>
+                        </div>
+                    </div>
                 </div>
             </div>
             <hr>
