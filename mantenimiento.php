bb0dc789a45b9c3eaa596dacb7659ad24dd3f397
Roberto Eder Weiss Juárez <weiss.uttab@gmail.com>
2016-05-26 01:24:31 -0500

    Format

--- mantenimiento.php
+++ mantenimiento.php
@@ -71,7 +71,7 @@
         <footer>
             <p class="text-center">&copy; Copyright <?php echo date("Y"); ?> | Corporativo Integral para Soluciones en Tiempo | Powered By <a href="http://webxico.blogspot.mx/" target="_blank">WEBXICO</a></p>
         </footer>
-    <script src="bower_components/jquery/dist/jquery.min.js"></script>
-    <script src="bower_components/bootstrap/dist/js/bootstrap.min.js"></script>
-</body>
+        <script src="bower_components/jquery/dist/jquery.min.js"></script>
+        <script src="bower_components/bootstrap/dist/js/bootstrap.min.js"></script>
+    </body>
 </html>
