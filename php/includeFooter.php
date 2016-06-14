14373782dec4c932efbce8ef32188fcbb54ad953
WEBXICO-MX <weiss.uttab@gmail.com>
2016-06-14 13:25:09 -0500

    Powered by

--- php/includeFooter.php
+++ php/includeFooter.php
@@ -1,3 +1,3 @@
 <footer>
-    <p class="text-center">&copy; Copyright <?php echo date("Y"); ?> | Corporativo Integral para Soluciones en Tiempo | Powered By <a href="http://webxico.blogspot.mx/" target="_blank">WEBXICO</a></p>
+    <p class="text-center">&copy; Copyright <?php echo date("Y"); ?> | Corporativo Integral para Soluciones en Tiempo | Powered by <a href="http://webxico.blogspot.mx/" target="_blank">WEBXICO</a></p>
 </footer>
