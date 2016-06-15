119055aac08b0a4c610621ac94f541c0acdac3b8
WEBXICO-MX <weiss.uttab@gmail.com>
2016-06-15 12:13:04 -0500

    Leé de base de datos la información de las categórias.

--- index.php
+++ index.php
@@ -1,5 +1,10 @@
 <?php
+require_once 'class/UtilDB.php';
 $origin = "";
+$sql = "";
+$rst = NULL;
+$html = "";
+$count = 0;
 ?>
 <!DOCTYPE html>
 <html lang="es">
@@ -24,69 +29,75 @@ $origin = "";
                     <div class="row">
                         <div class="col-md-12">
                             <div class="row">
-                                <div class="col-sm-6 col-md-4">
-                                    <h2>Desarrollo humano</h2>
-                                    <img src="img/desarrollo_humano.jpg" alt="Desarrollo humano" class="img-responsive"/><br/>
-                                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-                                    <p><a class="btn btn-success" href="php/capacitaciones.php?i=1" role="button"><span class="glyphicon glyphicon-calendar" style="font-size: 1.5em"></span> Ver capacitaciones &raquo;</a></p>
-                                    <hr>
-                                </div>
-                                <div class="col-sm-6 col-md-4">
-                                    <h2>Herramientas de calidad</h2>
-                                    <img src="img/herramientas_calidad.jpg" alt="Herramientas de calidad" class="img-responsive"/><br/>
-                                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-                                    <p><a class="btn btn-success" href="php/herramientas_calidad.php" role="button"><span class="glyphicon glyphicon-calendar" style="font-size: 1.5em" ></span> Ver capacitaciones &raquo;</a></p>
-                                    <hr>
-                                </div>
-                                <div class="clearfix visible-sm-block"></div>
-                                <div class="col-sm-6 col-md-4">
-                                    <h2>Lean manufacturing</h2>
-                                    <img src="img/lean_manufacturing.jpg" alt="Lean manufacturing" class="img-responsive"/><br/>
-                                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-                                    <p><a class="btn btn-success" href="php/lean_manufacturing.php" role="button"><span class="glyphicon glyphicon-calendar" style="font-size: 1.5em"></span> Ver capacitaciones &raquo;</a></p>
-                                    <hr>
-                                </div>
-                                <div class="clearfix visible-md-block visible-lg-block"></div>
-                                <div class="col-sm-6 col-md-4">
-                                    <h2>Seguridad industrial</h2>
-                                    <img src="img/seguridad_industrial.jpg" alt="Seguridad industrial" class="img-responsive"/><br/>
-                                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-                                    <p><a class="btn btn-success" href="php/seguridad_industrial.php" role="button"><span class="glyphicon glyphicon-calendar" style="font-size: 1.5em"></span> Ver capacitaciones &raquo;</a></p>
-                                    <hr>
-                                </div>
-                                <div class="clearfix visible-sm-block"></div>
-                                <div class="col-sm-6 col-md-4">
-                                    <h2>Integridad mecánica y aseguramiento de calidad</h2>
-                                    <img src="img/servicios_apoyo_pozos.jpg" alt="Servicio de apoyo a pozos" class="img-responsive"/><br/>
-                                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-                                    <p><a class="btn btn-success" href="php/integridad_mecanica.php" role="button"><span class="glyphicon glyphicon-calendar" style="font-size: 1.5em"></span> Ver capacitaciones &raquo;</a></p>
-                                    <hr>
-                                </div>
+                                <?php
+                                $sql = "SELECT * FROM categorias_capacitaciones WHERE activo = 1 AND id IN (1,2,3,4,5)";
+                                $rst = UtilDB::ejecutaConsulta($sql);
+
+                                if ($rst->rowCount() > 0) {
+                                    foreach ($rst as $row) {
+                                        $html .="<div class=\"col-sm-6 col-md-4 col-lg-4\">";
+                                        $html .="<h2>" . $row['nombre'] . "</h2>";
+                                        $html .="<img src=\"" . $row['img'] . "\" alt=\"" . $row['nombre'] . "\" class=\"img-responsive\"/><br/>";
+                                        $html .=$row['descripcion'];
+                                        $html .="<p><a class=\"btn btn-success\" href=\"php/capacitaciones.php?i=" . $row['id'] . "\" role=\"button\"><span class=\"glyphicon glyphicon-calendar\" style=\"font-size: 1.5em\"></span> Ver capacitaciones &raquo;</a></p>";
+                                        $html .="<hr>";
+                                        $html .="</div>";
+                                        $count++;
+
+                                        if ($count % 2 == 0) {
+                                            $html .="<div class=\"clearfix visible-sm-block\"></div>";
+                                        }
+                                        if ($count % 3 == 0) {
+                                            $html .="<div class=\"clearfix visible-md-block\"></div>";
+                                            $html .="<div class=\"clearfix visible-lg-block\"></div>";
+                                        }
+                                    }
+
+                                    echo($html);
+                                    $html = "";
+                                    $count = 0;
+                                } else {
+                                    
+                                }
+                                $rst->closeCursor();
+                                ?>
                             </div>
                             <div class="row">
                                 <div class="col-md-12">
                                     <h1>Perforación petrolera</h1><hr>
                                 </div>
-                                <div class="col-sm-6 col-md-4">
-                                    <h2>Perforación</h2>
-                                    <img src="img/perforacion.jpg" alt="PERFORACION" class="img-responsive"/><br/>
-                                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-                                    <p><a class="btn btn-success" href="php/perforacion.php" role="button"><span class="glyphicon glyphicon-calendar" style="font-size: 1.5em"></span> Ver capacitaciones &raquo;</a></p>
-                                </div>
-                                <div class="col-sm-6 col-md-4">
-                                    <h2> Mantenimiento a equipos de perforación</h2>
-                                    <img src="img/mantenimiento.jpg" alt=" Mantenimiento a equipos de perforación" class="img-responsive"/><br/>
-                                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-                                    <p><a class="btn btn-success" href="php/mantenimiento.php" role="button"><span class="glyphicon glyphicon-calendar" style="font-size: 1.5em"></span> Ver capacitaciones &raquo;</a></p>
-                                </div>
-                                <div class="clearfix visible-sm-block"></div>
-                                <div class="col-sm-6 col-md-4">
-                                    <h2>Servicio de apoyo a pozos</h2>
-                                    <img src="img/servicios_apoyo_pozos.jpg" alt="Servicio de apoyo a pozos" class="img-responsive"/><br/>
-                                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-                                    <p><a class="btn btn-success" href="php/servicio_apoyo_pozos.php" role="button"><span class="glyphicon glyphicon-calendar" style="font-size: 1.5em"></span> Ver capacitaciones &raquo;</a></p>
-                                </div>
-                                <div class="clearfix visible-md-block visible-lg-block"></div>                                
+                                <?php
+                                $sql = "SELECT * FROM categorias_capacitaciones WHERE activo = 1 AND id IN (6,7,8)";
+                                $rst = UtilDB::ejecutaConsulta($sql);
+
+                                if ($rst->rowCount() > 0) {
+                                    foreach ($rst as $row) {
+                                        $html .="<div class=\"col-sm-6 col-md-4 col-lg-4\">";
+                                        $html .="<h2>" . $row['nombre'] . "</h2>";
+                                        $html .="<img src=\"" . $row['img'] . "\" alt=\"" . $row['nombre'] . "\" class=\"img-responsive\"/><br/>";
+                                        $html .=$row['descripcion'];
+                                        $html .="<p><a class=\"btn btn-success\" href=\"php/capacitaciones.php?i=" . $row['id'] . "\" role=\"button\"><span class=\"glyphicon glyphicon-calendar\" style=\"font-size: 1.5em\"></span> Ver capacitaciones &raquo;</a></p>";
+                                        $html .="<hr>";
+                                        $html .="</div>";
+                                        $count++;
+
+                                        if ($count % 2 == 0) {
+                                            $html .="<div class=\"clearfix visible-sm-block\"></div>";
+                                        }
+                                        if ($count % 3 == 0) {
+                                            $html .="<div class=\"clearfix visible-md-block\"></div>";
+                                            $html .="<div class=\"clearfix visible-lg-block\"></div>";
+                                        }
+                                    }
+
+                                    echo($html);
+                                    $html = "";
+                                    $count = 0;
+                                } else {
+                                    
+                                }
+                                $rst->closeCursor();
+                                ?>
                             </div>
                         </div>
                     </div>
